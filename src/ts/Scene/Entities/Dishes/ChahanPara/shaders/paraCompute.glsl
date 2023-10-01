7e508291397bac096066526b7586640577534f4b
ukonpower <ore.ukonpower@gmail.com>
2023-10-02 00:21:59 +0900

    wip

--- src/ts/Scene/Entities/Dishes/ChahanPara/shaders/paraCompute.glsl
+++ src/ts/Scene/Entities/Dishes/ChahanPara/shaders/paraCompute.glsl
@@ -18,23 +18,45 @@ in vec2 vUv;
 void main( void ) {
 
 	float t = uTime * 0.8;
-	float id = vUv.x * uGPUResolution.x + vUv.y;
+	float id = ( vUv.x * uGPUResolution.x + vUv.y ) / uGPUResolution.x;
 
 	vec4 position = texture( gpuSampler0, vUv );
 	vec4 velocity = texture( gpuSampler1, vUv );
 
 	// velocity
 
-	vec3 noisePosition = position.xyz * 0.5 + id * (0.0001 + 0.005 * (1.0 - uParaState.x));
+	vec3 noisePosition = position.xyz * 0.6 + id * (0.5 + 0.1 * (1.0 - uParaState.x));
 	vec3 noise = fbm3( noisePosition + uTime ) - 0.45;
 
-	noise = noise * 0.005;
+	noise = noise * (0.002 + uParaState.x * 0.005);
 	velocity.xyz *= 0.99;
 	velocity.xyz += noise; 
 
-	vec3 gPos = position.xyz + vec3( 0.0, -2.0, 0.0 );
+	float r = (1.0 - uParaState.x) * 0.0004 ;
+
+	float dir = atan2( position.z, position.x );
+	velocity.x += sin( dir ) * r;
+	velocity.z += -cos( dir ) * r;
+
+	vec3 gravity = vec3( 0.00001 );
+
+	vec3 gPos = position.xyz + vec3( 0.0, -2.0 - 4.0 * ( 1.0 - uParaState.w ) , 0.0 );
+	gravity += gPos.xyz * smoothstep( 1.0, 3.0, length( gPos.xyz ) ) * -vec3(0.001, 0.005, 0.001) * ( 1.0 - smoothstep( 0.0, 1.0, -id + uParaState.x * 2.0 ) );
+
+	gPos = position.xyz + vec3( 0.0, -2.0, 0.0 );
+	gravity += gPos.xyz * smoothstep( 1.0, 3.0, length( gPos.xyz ) ) * -vec3(0.01) * uParaState.x * (1.0 - uParaState.z);
+
+	gPos = position.xyz + vec3( 0.0, 0.0, 0.0 );
+	gravity += gPos.xyz * smoothstep( 0.0, 0.01, length( gPos.xyz ) ) * -vec3(0.1) * smoothstep( 0.7, 1.0, -id + linearstep( 0.0, 1.0, uParaState.z) * 2.0 );
+
+	velocity.xyz += gravity;
+
+	if( length( velocity.xyz ) > 0.15 ) {
+
+		velocity.xyz = normalize( velocity.xyz ) * 0.15;
+
+	}
 	
-	velocity.xyz += gPos.xyz * smoothstep( 1.0, 3.0, length( gPos.xyz ) ) * -0.01 * ( uParaState.x * 0.9 + 0.01 );
 
 	//  position
 
